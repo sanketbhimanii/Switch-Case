b610c94c27fbd559d360ca5e0857be0c978dd6be
sanket <sanket.bhimani@exhibytesolution.com>
2022-10-04 18:46:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+ // 1.ways to print in java
+        // console.log("hello word");
+        // alert("me")
+        // document.write("this is document write");
+
+        // 2.javaScript console API
+        // console.log("Hello word",6+4,"another log");
+        // console.warn("this is warning");
+        // console.error("this is an error");         
+
+        // 3.JavaScript Variables 
+        var number1 = 20;
+        var number2 = 30;
+        console.log(number1 + number2);
+
+        // 4. data types in JavaScript  
+        // numbers
+        var num1 = 455;
+        var num2 = 451;
+
+        // string
+        var str1 = "This is String";
+        var str2 = "this is also string";
+        
+        // object
+        var marks={
+            sanket:52,
+            prit:45,
+            utsav:25
+        }
+        console.log(marks);
+        
+        // Boolians
+        var a = true;
+        var b = false;
+        console.log(a,b)
+
+        // var und=undefined;
+        var und; 
+        console.log(und);
+
+        var n = null;
+        console.log(n)
+
+        // arrey
+        var arr=[1,2,3,4,5,6,7,8,9,0]
+
+        
+
