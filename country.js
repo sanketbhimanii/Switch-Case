ea197f6ac870e2459ab2c78e82626efcd3a261f5
sanketbhimanii <sanket.bhimani@exhibytesolution.com>
2022-10-19 17:22:41 +0530

    sort/reverse

--- country.js
+++ country.js
@@ -2,46 +2,50 @@ const countries = [
   {
     name: "india",
     population: "10",
-    growthrate: "0.68% ",
+    growthrate: "3 ",
   },
   {
     name: "china",
     population: "11",
-    growthrate: "0.00%",
+    growthrate: "8",
   },
   {
     name: "brazil",
     population: "12",
-    growthrate: "0.46%",
+    growthrate: "4",
   },
   {
     name: "bahrain",
     population: "20",
-    growthrate: "0.61%",
+    growthrate: "2",
   },
 ];
 
-for (var a = 0; a < countries.length; a++) {
-  console.log(
-    countries[a].name +
-      " " +
-      countries[a].population +
-      " " +
-      countries[a].growthrate
-  );
-}
-countries.forEach(function (lrg) {
-  console.log(lrg);
-});
+// for (var a = countries.length; a >= 0; a--) {
+//   countries.sort();
+//   console.log(countries[a]);
+// }
 
-const myFunction =()=>  countries.filter((a1) => {
-    document.getElementById('btn').innerHTML = a1.population > 12;
-    // return 
-});
+// for (var a=countries.length; a<=0; a++) {
+  countries.reverse();
+  console.log(countries);
+// }
 
-console.log("filter", myFunction());
 
-let map = countries.map(function (a1) {
-return a1.growthrate <= 0.5;
-});
-console.log(map);
+// const abc = countries.filter((ab) => {
+//   return ab.population >= 10;
+// });
+// console.log("filter", abc);
+
+// abc.sort((a, b) => (a.name > b.name ? 1 : -1));
+// console.log("sort", abc);
+
+// const xyz = countries.filter((ab) => {
+//   return ab.growthrate >=2;
+// });
+// console.log("filter", xyz);
+
+// xyz.sort((e, f) => (e.growthrate < f.growthrate ? 1 : -1));
+// console.log("sort", xyz); 
+
+ 
